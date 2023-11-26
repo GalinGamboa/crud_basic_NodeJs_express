a054e1349a2e843ca9aa8cbc0a3df33a728ec39e
Galo Gamboa <galin_71@hotmail.com>
2023-11-26 07:31:54 +0100

    Créez une table dans index.ejs et transmettez un array

--- Router/routerPages.js
+++ Router/routerPages.js
@@ -2,7 +2,15 @@ import express from 'express';
 export const router = express.Router();
 
 router.get('/',(req,res)=>{
-   res.render('index.ejs')
-});
+   res.render('index.ejs',{
+      arrayAnimaux : [
+            {id:'01', nom:'Coco',description:'Coco descricption'},
+            {id:'02', nom:'Gasper',description:'Gasper descricption'},
+            {id:'01', nom:'Michi',description:'Michi descricption'},
+            {id:'02', nom:'Scooby',description:'Scooby descricption'}
+      ]
+   })
+})
+
 
 
